f2937d7d8060021a824944017a208157085ad4b1
rungefuk <roman.ungefuk@gmail.com>
2020-07-02 17:25:26 +0200

    added some improvments

--- src/homework5/CalcTask.java
+++ src/homework5/CalcTask.java
@@ -16,26 +16,19 @@ public class CalcTask implements Runnable {
         int h = sizeArray / 2;
         float[] a1 = new float[h];
         float[] a2 = new float[h];
+
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
         Thread tA1 = new Thread(() -> {
-            for (int i = 0; i < a1.length; i++) {
-                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5)
-                        * Math.cos(0.4f + i / 2));
-            }
+            calcValuesArrayInThread(a1);
         });
         tA1.start();
 
-        Thread tA2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < a2.length; i++) {
-                    a2[i] = (float) (a2[i] * Math.sin(0.2f + h / 5) * Math.cos(0.2f + h / 5)
-                            * Math.cos(0.4f + h / 2));
-                }
-            }
+        Thread tA2 = new Thread(() -> {
+            calcValuesArrayInThread(a2);
         });
         tA2.start();
+
         if (tA1.isAlive() || tA2.isAlive()) {
             try {
                 tA1.join();
@@ -70,5 +63,12 @@ public class CalcTask implements Runnable {
         thread.start();
         thread.join();
 
+    }
+
+    public void calcValuesArrayInThread(float[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5)
+                    * Math.cos(0.4f + i / 2));
+        }
     }
 }
